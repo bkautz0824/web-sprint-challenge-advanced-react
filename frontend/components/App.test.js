3034328e5c1882506388ea1e2d48d91ab99f6b92
Bennett Kautz <bennettkautz@gmail.com>
2022-05-26 16:00:03 -0400

    tests

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,18 +1,19 @@
 // Write your tests here
 import React from 'react'
-import AppFunctional from './frontend/components/AppFunctional'
+import AppFunctional from './AppFunctional'
 import { render, fireEvent, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
 
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+// test('sanity', () => {
+//   expect(true).toBe(false)
+// })
 
 
 test('texts are visible on keypad', () => {
   render(<AppFunctional/>)
 
-  const info = screen.getByText('You moved')
+  const keypad = screen.getByText('LEFT')
 
+  expect(keypad).toBeInTheDocument();
 })
